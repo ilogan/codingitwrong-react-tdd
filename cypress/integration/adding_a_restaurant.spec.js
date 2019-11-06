1674c62c9cc71b09f015428b448cd8c5e24a6ead
Ian Logan <itlogan9@gmail.com>
2019-11-06 10:29:36 -0600

    Fix label in front of input error

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -12,7 +12,9 @@ describe("adding a restaurant", () => {
     cy.get("[data-testid='addRestaurantButton']").click();
 
     // an input box pops up, so they enter the name of their favorite sushi place
-    cy.get("[data-testid='newRestaurantName']").type(restaurantName);
+    cy.get("[data-testid='newRestaurantName']").type(restaurantName, {
+      force: true
+    });
 
     // they click save
     cy.get("[data-testid='saveNewRestaurantButton']").click();
