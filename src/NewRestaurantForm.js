f5229148f23205a1dc8806118704a44620d07f1c
Ian Logan <itlogan9@gmail.com>
2019-11-12 13:12:24 -0600

    Extract form rendering to component

--- src/NewRestaurantForm.js
+++ src/NewRestaurantForm.js
@@ -18,39 +18,40 @@ function NewRestaurantForm({ onSave }) {
     return errors;
   };
 
+  const RestaurantForm = ({ values, errors, handleChange, handleSubmit }) => (
+    <form onSubmit={handleSubmit}>
+      <TextInput
+        s={12}
+        m={8}
+        l={10}
+        className={errors.restaurantName ? "invalid" : ""}
+        label="Restaurant Name"
+        name="restaurantName"
+        value={values.restaurantName}
+        error={errors.restaurantName}
+        onChange={handleChange}
+        data-testid="newRestaurantName"
+      />
+      <Button
+        s={12}
+        m={4}
+        l={2}
+        data-testid="saveNewRestaurantButton"
+        type="submit"
+      >
+        Save
+      </Button>
+    </form>
+  );
+
   return (
     <Row>
       <Formik
         initialValues={{ restaurantName: "" }}
         onSubmit={handleSave}
         validate={validate}
-      >
-        {({ values, errors, handleChange, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <TextInput
-              s={12}
-              m={8}
-              l={10}
-              className={errors.restaurantName ? "invalid" : ""}
-              label="Restaurant Name"
-              name="restaurantName"
-              value={values.restaurantName}
-              error={errors.restaurantName}
-              onChange={handleChange}
-              data-testid="newRestaurantName"
-            />
-            <Button
-              s={12}
-              m={4}
-              l={2}
-              data-testid="saveNewRestaurantButton"
-              type="submit"
-            >
-              Save
-            </Button>
-          </form>
-        )}
-      </Formik>
+        component={RestaurantForm}
+      />
     </Row>
   );
 }
