c0908c0c9237ef1c9f06ae0d5127ce4969a25310
Ian Logan <itlogan9@gmail.com>
2019-11-12 08:59:25 -0600

    Refactor to use formik

--- src/NewRestaurantForm.js
+++ src/NewRestaurantForm.js
@@ -1,36 +1,42 @@
 import React, { useState } from "react";
 
 import { Button, TextInput, Row } from "react-materialize";
+import { Formik } from "formik";
 
 function NewRestaurantForm({ onSave }) {
-  const [inputText, setInputText] = useState("");
-
-  const handleSave = e => {
-    e.preventDefault();
-    onSave(inputText);
-    setInputText("");
+  const handleSave = (values, { resetForm }) => {
+    const { restaurantName } = values;
+    onSave(restaurantName);
+    resetForm();
   };
 
   return (
     <Row>
-      <TextInput
-        s={12}
-        m={8}
-        l={10}
-        label="Restaurant Name"
-        value={inputText}
-        onChange={e => setInputText(e.target.value)}
-        data-testid="newRestaurantName"
-      />
-      <Button
-        s={12}
-        m={4}
-        l={2}
-        data-testid="saveNewRestaurantButton"
-        onClick={handleSave}
-      >
-        Save
-      </Button>
+      <Formik initialValues={{ restaurantName: "" }} onSubmit={handleSave}>
+        {({ values, handleChange, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextInput
+              s={12}
+              m={8}
+              l={10}
+              label="Restaurant Name"
+              name="restaurantName"
+              value={values.restaurantName}
+              onChange={handleChange}
+              data-testid="newRestaurantName"
+            />
+            <Button
+              s={12}
+              m={4}
+              l={2}
+              data-testid="saveNewRestaurantButton"
+              type="submit"
+            >
+              Save
+            </Button>
+          </form>
+        )}
+      </Formik>
     </Row>
   );
 }
