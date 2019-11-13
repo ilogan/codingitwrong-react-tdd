9d572651647ebd218a97cc96a89e952bb4001935
Ian Logan <itlogan9@gmail.com>
2019-11-13 10:17:28 -0600

    Refactor to render app through react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 import { Row, Col } from "react-materialize";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import RestaurantListPage from "./RestaurantListPage";
 
 function App() {
   return (
-    <Row>
-      <Col s={12} m={10} l={8} offset="m1 l2">
-        <RestaurantListPage />
-      </Col>
-    </Row>
+    <Router>
+      <Row>
+        <Col s={12} m={10} l={8} offset="m1 l2">
+          <Route path="/" exact component={RestaurantListPage} />
+        </Col>
+      </Row>
+    </Router>
   );
 }
 
